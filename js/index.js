db01d323afc7622b22eacf9b6bdbf22746ff1369
MugeraH <bwosihughes24@gmail.com>
2021-03-06 20:53:40 +0300

    add jquery.stapel plugin for image gallery,setup image gallery and styling

--- js/index.js
+++ js/index.js
@@ -1,7 +1,38 @@
 $(document).ready(function() {
-  $('#order').click(()=>{
-        $('.aside-section').toggleClass('aside-section_active')
+
+
+  $(function(){
+    let $grid = $("#tp-grid"),
+        $name = $('#name'),
+        $close = $('#close'),
+        $loader = $('').insertBefore($grid),
+        stapel = $grid.stapel({
+          randomAngle: true,
+          delay:50,
+          gutter: 80,
+          pileAngles : 1,
+          onLoad: function(){
+            $loader.remove();
+          },
+          onBeforeOpen : function(pileName){
+             $name.html(pileName);
+          },
+          onAfterOpen : function (pileName){
+            $close.show(500);
+          }
+        });
+  
+      $close.on('click',function(){
+        $close.hide();
+        $name.empty();
+        stapel.closePile();
+      });  
   })
+  
+  // $('#order').click(()=>{
+  //       $('.aside-section').toggleClass('aside-section_active')
+  // })
 
 
 })
+
