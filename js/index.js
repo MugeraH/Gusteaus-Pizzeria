1d29470785baf958eedf9fcb687ea1c26cfd4924
MugeraH <bwosihughes24@gmail.com>
2021-03-07 02:06:59 +0300

    fix modal display bug by increasing its z-index to make it display over other elements

--- js/index.js
+++ js/index.js
@@ -7,20 +7,18 @@ var myModal = new bootstrap.Modal(document.getElementById('myModal'))
 $(document).ready(function() {
 
 
-  $('#myBtn').click(()=>{
-    myModal.show()
-    console.log('hm');
-  })
+
 
   $(function(){
-    let $grid = $("#tp-grid"),
+  
+    var $grid = $("#tp-grid"),
         $name = $('#name'),
         $close = $('#close'),
         $loader = $('').insertBefore($grid),
         stapel = $grid.stapel({
-          randomAngle: true,
+          randomAngle: false,
           delay:50,
-          gutter: 80,
+          gutter: 70,
           pileAngles : 1,
           onLoad: function(){
             $loader.remove();
@@ -30,15 +28,31 @@ $(document).ready(function() {
           },
           onAfterOpen : function (pileName){
             $close.show(500);
+            $('#back').show()
           }
         });
+
+        $('#back').click(()=>{
+          $close.hide();
+         $name.empty();
+         stapel.closePile();
+         $('#back').hide()
+        })
   
-      $close.on('click',function(){
-        $close.hide();
-        $name.empty();
-        stapel.closePile();
-      });  
+
+        
+      // $close.on('click',function(){
+      //   $close.hide();
+      //   $name.empty();
+      //   stapel.closePile();
+      // });  
   })
 
+
+
+  $('#myBtn').click(()=>{
+    myModal.show()
+
+  })
 })
 
