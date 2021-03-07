d1d99893d4fbbbe639996a6b0477e789cf931e61
MugeraH <bwosihughes24@gmail.com>
2021-03-08 02:14:44 +0300

    removed image slider

--- js/index.js
+++ js/index.js
@@ -234,36 +234,7 @@ $('#myBtn').click(()=>{
 
 
 
-  $(function(){
-      var $grid = $("#tp-grid"),
-        $name = $('#name'),
-        $close = $('#close'),
-        $loader = $('').insertBefore($grid),
-        stapel = $grid.stapel({
-          randomAngle: false,
-          delay:50,
-          gutter: 50,
-          pileAngles : 1,
-          onLoad: function(){
-            $loader.remove();
-          },
-          onBeforeOpen : function(pileName){
-            //  $name.html(pileName);
-          },
-          onAfterOpen : function (pileName){
-            $close.show(500);
-            $('#back').show()
-          }
-        });
 
-        $('#back').click(()=>{
-          $close.hide();
-         $name.empty();
-         stapel.closePile();
-         $('#back').hide()
-        })
-  
-  })
 
 
 
